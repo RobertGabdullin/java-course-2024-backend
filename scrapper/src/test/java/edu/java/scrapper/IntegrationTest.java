f06006a083e232c070c2da56ac92104838d1dc4a
Robert <lol.sos2014@yandex.ru>
2024-04-10 19:49:07 +0300

    hw4fix

--- scrapper/src/test/java/edu/java/scrapper/IntegrationTest.java
+++ scrapper/src/test/java/edu/java/scrapper/IntegrationTest.java
@@ -1,28 +1,15 @@
 package edu.java.scrapper;
 
-import liquibase.Contexts;
-import liquibase.LabelExpression;
 import liquibase.Liquibase;
 import liquibase.database.Database;
 import liquibase.database.DatabaseFactory;
 import liquibase.database.jvm.JdbcConnection;
-import liquibase.exception.LiquibaseException;
-import liquibase.resource.ClassLoaderResourceAccessor;
-import liquibase.resource.FileSystemResourceAccessor;
 import liquibase.resource.SearchPathResourceAccessor;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.JdbcDatabaseContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import javax.sql.DataSource;
-import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public abstract class IntegrationTest {
     public static PostgreSQLContainer<?> POSTGRES;
--- scrapper/src/test/java/edu/java/scrapper/PostgresTest.java
+++ scrapper/src/test/java/edu/java/scrapper/PostgresTest.java
@@ -2,13 +2,6 @@ package edu.java.scrapper;
 
 import org.junit.jupiter.api.Test;
 
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PostgresTest extends IntegrationTest {
